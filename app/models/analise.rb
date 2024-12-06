0c87447cdf970044822445f365c9349e12d06f2b
Isaac Ribeiro <137534765+isaacribeirodev@users.noreply.github.com>
2024-12-06 04:43:40 +0000

    fix

--- app/models/analise.rb
+++ app/models/analise.rb
@@ -1,5 +1,5 @@
 class Analise < ApplicationRecord
   belongs_to :funcionario
 
-  validates :processo, :status_analise, :funcionario_id, presence: true
+  validates :processo, :status_analise, :funcionario, presence: true
 end
